d1ff089ef432e3750e148b88b1d077dcb4fde990
acmerfight <acmerfight@gmail.com>
2012-09-13 10:35:49 +0800

    send email

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,18 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the rails application
 Depot::Application.initialize!
+
+Depot::Application.configure do
+
+  config.action_mailer.delivery_method = :smtp 
+
+  config.action_mailer.smtp_settings = {
+    :address => "smtp.gmail.com",
+    :port    => 587,
+    :domain  => 'baci.lindsaar.net',
+    :user_name    => 'acmerfight@gmail.com',
+    :password => 'pandayujing1223',
+    :authentication => 'plain',
+    :enable_starrttls_auto => true
+  }
+end
